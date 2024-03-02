6316f6b700e0efcf86b6eff8eac5388cd8a145d5
Chhavi Prabhakar <prabhakarchhavi9@gmail.com>
2024-03-02 22:49:57 +0530

    seive Of Eratosthenes

---
+++ other/seiveOfEratosthenes.js
@@ -0,0 +1,21 @@
+function seiveOfEratosthenes(n) {
+  let isPrime = new Array(n + 1).fill(true);
+  isPrime[0] = isPrime[1] = false;
+  for (let i = 2; i * i <= n; i++) {
+    if (isPrime[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        isPrime[j] = false;
+      }
+    }
+  }
+
+  let primes = [];
+  for (let i = 2; i <= n; i++) {
+    if (isPrime[i]) {
+      primes.push(i);
+    }
+  }
+  return primes;
+}
+
+console.log(seiveOfEratosthenes(100));
